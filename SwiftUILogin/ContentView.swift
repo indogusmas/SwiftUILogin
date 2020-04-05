7d3f2484fc81533568a38a7585c124cda6102080
indogusmas <indogusmasarungsamudra@gmail.com>
2020-04-05 09:01:32 +0700

    Initial Commit

---
+++ SwiftUILogin/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUILogin
+//
+//  Created by indo gusmas arung samudra on 05/04/20.
+//  Copyright © 2020 indo gusmas arung samudra. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
